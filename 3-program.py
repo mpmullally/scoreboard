189c8348f2d11866774a27ec74decf04b566a71a
mpmullally <michael.p.mullally@gmail.com>
2018-03-08 18:55:06 -0500

    updated number

--- 3-program.py
+++ 3-program.py
@@ -28,7 +28,7 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BCM)     # set up BCM GPIO numbering
 
 # Set up input pin
-GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 # Callback function to run in another thread when button pressed
 def buttonPressed(channel):
