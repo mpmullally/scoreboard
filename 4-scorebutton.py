f3acafd32a16994c6152b6b01dd0f8a78d8cbd07
Michael Mullally <michael.p.mullally@gmail.com>
2018-03-08 15:58:32 -0500

    Update 4-scorebutton.py

--- 4-scorebutton.py
+++ 4-scorebutton.py
@@ -29,66 +29,81 @@ GPIO.setup(13, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(6, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.setup(5, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-global bluecounter = 0
-global redcounter = 0
+bluecounter = 0
+redcounter = 0
 # Callback function to run in another thread when button pressed
 def RedPlus(channel):
     lcd.clear()
     global redcounter
+    global bluecounter
     if redcounter <21:
         redcounter += 1
         lcd.message('Red: {0}'.format(redcounter)'\n Blue: {0}'.format(bluecounter) )
     else:
         lcd.message('RED WINS!!!')
+        lcd.clear()
+        lcd.message('New Game!!!')
+        sleep(2)
+        lcd.clear()
+        redcounter = 0
+        bluecounter = 0
+        lcd.message('Red: {0}'.format(redcounter)'\n Blue: {0}'.format(bluecounter) )
 # Callback function to run in another thread when button pressed
 def RedReset(channel):
     lcd.clear()
     global redcounter
+    global bluecounter
     redcounter = 11
-    lcd.message('Red: {0}'.format(redcounter))
+    lcd.message('Red: {0}'.format(redcounter)'\n Blue: {0}'.format(bluecounter) )
 # Callback function to run in another thread when button pressed
 def BluePlus(channel):
     lcd.clear()
+    global redcounter
     global bluecounter
     if bluecounter <21:
         redcounter += 1
         lcd.message('Red: {0}'.format(redcounter)'\n Blue: {0}'.format(bluecounter) )
     else:
         lcd.message('BLUE WINS!!!')
+        lcd.clear()
+        lcd.message('New Game!!!')
+        sleep(2)
+        lcd.clear()
+        redcounter = 0
+        bluecounter = 0
+        lcd.message('Red: {0}'.format(redcounter)'\n Blue: {0}'.format(bluecounter) )
 # Callback function to run in another thread when button pressed
 def BlueReset(channel):
     lcd.clear()
+    global redcounter
     global bluecounter
     counter = 11
-    lcd.message('Button Pressed\n{0}'.format(counter))
+    lcd.message('Red: {0}'.format(redcounter)'\n Blue: {0}'.format(bluecounter) )
 # Callback function to run in another thread when button pressed
-def Reset(channel):
-    lcd.clear()
-    lcd.message('New Game!!!')
-    sleep(2)
-    lcd.clear()
-    lcd.message('Red: 0 \nBlue: 0')
+# def Reset(channel):
+     #lcd.clear()
+     #lcd.message('New Game!!!')
+     #sleep(2)
+     #lcd.clear()
+     #global redcounter == 0
+     #global bluecounter == 0
+     #lcd.message('Red: {0}'.format(redcounter)'\n Blue: {0}'.format(bluecounter) )
 
 # add event listener on pin 26
 # event will interrupt the program and call the buttonPressed function
-GPIO.add_event_detect(26, GPIO.RISING, callback=RedPlus, bouncetime=150)
-counter = 0
+GPIO.add_event_detect(26, GPIO.FALLING, callback=RedPlus, bouncetime=150)
 # add event listener on pin 19
 # event will interrupt the program and call the buttonPressed function
-GPIO.add_event_detect(19, GPIO.RISING, callback=RedReset, bouncetime=150)
-counter = 0
+GPIO.add_event_detect(19, GPIO.FALLING, callback=RedReset, bouncetime=150)
 # add event listener on pin 13
 # event will interrupt the program and call the buttonPressed function
-GPIO.add_event_detect(13, GPIO.RISING, callback=BluePlus, bouncetime=150)
-counter = 0
+GPIO.add_event_detect(13, GPIO.FALLING, callback=BluePlus, bouncetime=150)
 # add event listener on pin 6
 # event will interrupt the program and call the buttonPressed function
-GPIO.add_event_detect(6, GPIO.RISING, callback=BlueReset, bouncetime=150)
-counter = 0
+GPIO.add_event_detect(6, GPIO.FALLING, callback=BlueReset, bouncetime=150)
 # add event listener on pin 5
 # event will interrupt the program and call the buttonPressed function
-GPIO.add_event_detect(5, GPIO.RISING, callback=Reset, bouncetime=150)
-counter = 0
+#GPIO.add_event_detect(5, GPIO.FALLING, callback=Reset, bouncetime=150)
 
 try:
     while True:
