09060489d7127720d7f5ac0cd0dae4cd1f393723
Michael Mullally <michael.p.mullally@gmail.com>
2018-03-09 13:12:48 -0500

    Update scoreboard.py

--- scoreboard.py
+++ scoreboard.py
@@ -1,15 +1,37 @@
+from Adafruit_CharLCD import Adafruit_CharLCD
+
+# Raspberry Pi pin configuration:
+lcd_rs        = 25
+lcd_en        = 24
+lcd_d4        = 23
+lcd_d5        = 17
+lcd_d6        = 18
+lcd_d7        = 22
+lcd_backlight = 4
+
+# Define LCD column and row size for 16x2 LCD.
+lcd_columns = 16
+lcd_rows    = 2
+
+# Initialize the LCD using the pins above.
+lcd = LCD.Adafruit_CharLCD(lcd_rs, lcd_en, lcd_d4, lcd_d5, lcd_d6, lcd_d7,
+                           lcd_columns, lcd_rows, lcd_backlight)
+lcd.clear()
+
 RedScore = 0
 BlueScore = 0
 
 from time import sleep
 while RedScore <= 5 and BlueScore <= 5:
     if RedScore == 5:
-        print('RED WINS')
+        lcd.clear()
+        lcd.message('RED WINS!!!')
         sleep(2)
         RedScore = BlueScore = 0
         continue
     elif BlueScore == 5:
-        print('BLUE WINS')
+        lcd.clear()
+        lcd.message('BLUE WINS!!!')
         sleep(2)
         BlueScore = RedScore = 0
         continue
@@ -17,28 +39,20 @@ while RedScore <= 5 and BlueScore <= 5:
         x = input("Who Scored? ")
         if x == 'Red':
             RedScore += 1
-            print(RedScore)
+            lcd.clear()
+            lcd.message('Red: 'RedScore'  Blue:'BlueScore)
         elif x == 'Blue':
             BlueScore += 1
-            print(BlueScore)
+            lcd.clear()
+            lcd.message('Red: 'RedScore'  Blue:'BlueScore)
         elif x == 'REDRESET':
             RedScore = 3
-            print(RedScore)
+            lcd.clear()
+            lcd.message('Red: 'RedScore'  Blue:'BlueScore)
         elif x == 'BLUERESET':
             BlueScore = 3
-            print(BlueScorecore)
+            lcd.clear()
+            lcd.message('Red: 'RedScore'  Blue:'BlueScore)
         else:
-            print('Bad Input')
-
-
-#import RPi.GPIO as GPIO
-
-#GPIO.setmode(GPIO.BCM)
-
-#GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-#while True:
-#input_state = GPIO.input(18)
-#if input_state == False:
-#RedScore += 1
-#time.sleep(0.2)
+            lcd.clear()
+            lcd.message('Bad Input')
